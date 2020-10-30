c3167cec9263c04a5efd5be94335e39338c354c6
xworkzodc <48981565+xworkzodc@users.noreply.github.com>
2020-10-30 10:02:26 +0530

    Add files via upload

---
+++ test-branch/Vodafone.java
@@ -0,0 +1,40 @@
+class Wish
+{
+// this is comments
+//To start a program we need a method(main)
+//main method is an entry point
+public static void main(String[] hanumesh)
+{
+   
+System.out.println("Namaskara");   
+}
+
+}
+class Camera{public static void main(String camera[]){
+	System.out.println("Camera is Kodak");;
+	System.out.print("price is lot");;
+}}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+

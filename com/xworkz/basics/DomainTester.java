687275763938c0a64111bbe0bfdf39d4b6c0cffe
xworkzodc <48981565+xworkzodc@users.noreply.github.com>
2020-11-10 10:05:27 +0530

    Add files via upload

---
+++ com/xworkz/basics/DomainTester.java
@@ -0,0 +1,19 @@
+package com.xworkz.basics;
+
+public class DomainTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      //ctrl+space
+		//ctrl+ f11
+		System.out.println("This is my first eclispe run");
+		String name=args[0];
+		System.out.println(name);
+		String name1=args[1];
+		System.out.println(name1);
+		System.out.println("Exit :: Main");
+	}
+	
+}
+
+
---
+++ com/xworkz/basics/STDCode.java
@@ -0,0 +1,7 @@
+package com.xworkz.basics;
+
+public enum STDCode {
+	
+	080,083
+
+}

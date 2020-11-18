397a285eff4b2824f9a9d535010d9a75506db2e2
ombn <om.bn@outlook.com>
2020-11-18 12:19:16 +0530

    packages and access specifier

---
+++ laptop/src/com/xworkz/laptop/Tester.java
@@ -0,0 +1,20 @@
+package com.xworkz.laptop;
+
+import com.xworkz.laptop.company.Admin;
+import com.xworkz.laptop.company.Lenovo;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		
+		
+		Admin.develop();
+		
+		
+		
+		Lenovo.getModelNumber();
+		
+		
+	}
+
+}
---
+++ laptop/src/com/xworkz/laptop/company/Lenovo.java
@@ -0,0 +1,18 @@
+package com.xworkz.laptop.company;
+
+public class Lenovo {
+	
+	
+	private static String modelNumber="MCarbon";
+
+	 static void developApplication() {
+		System.out.println("invoked developApplication");
+	}
+	
+	
+	public static String getModelNumber()
+	{
+		return modelNumber;
+	}
+
+}
